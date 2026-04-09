05d0c3fc2d2f7181fd1dde3951b108e55decca3e
Rinaldo T.S Filho <rinaldotavares12@gmail.com>
2026-04-09 20:14:28 -0300

    Add files via upload

---
+++ Práticas Esp32/pratica_4_2.ino
@@ -0,0 +1,18 @@
+#include <ESP32Servo.h> 
+
+static const int servoPin = 12;
+static const int potentiometerPin = 13;
+
+Servo servo1;
+
+void setup() {
+    Serial.begin(115200);
+    servo1.attach(servoPin);
+}
+
+void loop() {
+    int servoPosition = map(analogRead(potentiometerPin), 0, 4096, 0, 180);
+    servo1.write(servoPosition);
+    Serial.println(servoPosition);
+    delay(20);
+}
